44b305122c408164ce648c1289ecb8950455c470
Shad-islam <shadislam48@gmail.com>
2023-10-30 05:02:47 +0600

    Create NoteForm component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
+import { useState } from "react";
+import NoteForm from "./component/NoteForm";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [update, setUpdate] = useState(null);
+  const [noteTitle, setNoteTitle] = useState("");
   return (
     <div className="App">
       <h1>Hello! Clean project folder and ready to set real version </h1>
+      <NoteForm
+        update={update}
+        setEdit={setEdit}
+        setNotes={setNotes}
+        noteTitle={noteTitle}
+        setNoteTitle={setNoteTitle}
+      />
     </div>
   );
 }
